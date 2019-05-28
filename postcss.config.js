b1d0d53b43b581598e7bba1c7cd62f60b1d7a296
“richardl33” <rich.leeroy@gmail.com>
2019-05-28 14:45:57 -0400

    npm build commands

---
+++ postcss.config.js
@@ -0,0 +1,27 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+   plugins: [
+      tailwindcss('./tailwind.config.js'),
+      cssnano({
+         preset: 'default'
+      }),
+      purgecss({
+         content: ['./src/*.html'],
+         extractors: [
+            {
+               extractor: class TailwindExtractor {
+                  static extract(content) {
+                     return content.match(/[A-z0-9-:\/]+/g) || [];
+                  }
+               },
+               extensions: ['css','html']
+            }
+         ]
+      }),
+      autoprefixer
+   ]
+}
